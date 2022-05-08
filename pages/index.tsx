766ca76aedd13573f7153b61589fe832da35b646
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-05-08 07:37:29 +0700

    move some design to default layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,15 +13,12 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div style={{ padding: 100 }}>
-      <h1>Next Event</h1>
-      <MainMenu />
-      <div style={{ padding: 30 }}>
-        <Card title="Event name"  style={{ width: '100%', borderRadius: 5 }}>
-          <h4>Start: 10 Feb 2022</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et diam mi. In commodo est a odio varius suscipit. Quisque ac urna in odio posuere rhoncus. Suspendisse ornare arcu nec pretium ultricies. Nullam scelerisque, lacus sit amet aliquet scelerisque, libero eros pretium dui, a hendrerit lacus nulla non risus. Suspendisse vel congue dui, vitae volutpat ipsum. Morbi aliquam facilisis orci. Mauris et risus tempor, scelerisque quam vitae, auctor elit. Etiam vel odio ante. Pellentesque orci turpis, posuere non auctor vel, auctor at velit. Phasellus suscipit elit non velit ultrices maximus. Aliquam sagittis elit vel dignissim facilisis. Maecenas suscipit ut tellus id tincidunt. Maecenas vulputate tempor condimentum.</p>
-        </Card>
-      </div>
+
+    <div style={{ padding: 30 }}>
+      <Card title="Event name" style={{ width: '100%', borderRadius: 5 }}>
+        <h4>Start: 10 Feb 2022</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et diam mi. In commodo est a odio varius suscipit. Quisque ac urna in odio posuere rhoncus. Suspendisse ornare arcu nec pretium ultricies. Nullam scelerisque, lacus sit amet aliquet scelerisque, libero eros pretium dui, a hendrerit lacus nulla non risus. Suspendisse vel congue dui, vitae volutpat ipsum. Morbi aliquam facilisis orci. Mauris et risus tempor, scelerisque quam vitae, auctor elit. Etiam vel odio ante. Pellentesque orci turpis, posuere non auctor vel, auctor at velit. Phasellus suscipit elit non velit ultrices maximus. Aliquam sagittis elit vel dignissim facilisis. Maecenas suscipit ut tellus id tincidunt. Maecenas vulputate tempor condimentum.</p>
+      </Card>
     </div>
   )
 }
