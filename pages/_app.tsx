a9265d9b05e4a9da9d51328558940dae8679d116
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-05-11 21:17:39 +0700

    setup web3 provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,25 @@ import 'antd/dist/antd.css';
 import type { AppProps } from 'next/app'
 import MainMenu from '../components/menu';
 
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div style={{ padding: 100 }}>
-      <h1>Next Event</h1>
-      <MainMenu />
-      <Component {...pageProps} />
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div style={{ padding: 100 }}>
+        <h1>Next Event</h1>
+        <MainMenu />
+        <Component {...pageProps} />
+      </div>
+    </Web3ReactProvider>
   )
 }
 
